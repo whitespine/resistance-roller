ee912248fb1faefdd841324e560898eaeefc84cd
WHITESPINE <jakehenry.tn@gmail.com>
2022-12-11 19:10:21 +0000

    Fixed messages to properly infer type

--- src/view/chat/RollResultMessage.svelte
+++ src/view/chat/RollResultMessage.svelte
@@ -1,23 +1,25 @@
 <!-- Super simple Svelte component that takes in a prop and outputs it. -->
 <script>
     /** @type {RollResultMessageFlags} */
-    export let roll = null;
+    export let rollData = null;
     /** @type {Actor | null} */
-    $: leader = (roll.actor_id ? fromUuidSync(roll.actor_id) : null); 
+    $: leader = fromUuidSync(rollData.actor_id);
 </script>
 
 <div class="flexcol">
     <div class="result">
-        <h2>{leader?.name ?? "Unknown Character's"} roll:</h2>
-        <span>Dice: {(roll?.rolls ?? [0]).join(", ")}</span>
-        <span>Result: {roll?.roll ?? 0}</span>
+        <h2>{leader?.name ?? "Unknown Character"}'s roll:</h2>
+        <span>Dice: {(rollData?.rolls ?? [0]).join(", ")}</span>
+        <span>Result: {rollData?.roll ?? 0}</span>
     </div>
 </div>
 
 <style lang="scss">
     .result {
+        /*
         display: grid;
         grid-template-rows: 32px 1fr;
         grid-template-columns: 32px 1fr 1fr;
+        */
     }
 </style>
